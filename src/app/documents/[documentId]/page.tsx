efc226223c6623e039d78cb0b9bacbcdf9299677
Nirmalya Nayak <n.nayak263@gmail.com>
2025-01-01 18:20:11 +0530

    add Zustand for state management and implement toolbar component in document page

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 export default async function DocumentIdPage({
   params,
@@ -9,6 +10,7 @@ export default async function DocumentIdPage({
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
