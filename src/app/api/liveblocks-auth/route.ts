6b673ab5d8ca81833366feaff002a73b133e3569
Nirmalya Nayak <n.nayak263@gmail.com>
2025-01-20 10:42:21 +0530

    feat: implement user fetching and enhance user resolution in Liveblocks provider

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -29,7 +29,8 @@ export async function POST(req: Request) {
 
   const session = liveblocks.prepareSession(user.id, {
     userInfo: {
-      name: user.fullName ?? "Anonymus",
+      name:
+        user.fullName ?? user.primaryEmailAddress?.emailAddress ?? "Anonymous",
       avatar: user.imageUrl,
     },
   });
--- src/app/documents/[documentId]/room.tsx
+++ src/app/documents/[documentId]/room.tsx
@@ -5,8 +5,12 @@ import {
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import { toast } from "sonner";
+import { useEffect, useMemo, useState } from "react";
 
 import { FullscreenLoader } from "@/components/fullscreen-loader";
+
+import { getUsers } from "./actions";
 
 export function Room({
   children,
@@ -15,8 +19,48 @@ export function Room({
   children: React.ReactNode;
   documentId: string;
 }) {
+  const [users, setUsers] = useState<
+    { id: string; name: string; avatar: string }[]
+  >([]);
+
+  const fetchUsers = useMemo(
+    () => async () => {
+      try {
+        const usersList = await getUsers();
+        setUsers(usersList);
+      } catch {
+        toast.error("Failed to fetch users");
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
   return (
-    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+    <LiveblocksProvider
+      throttle={16}
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={({ userIds }) =>
+        userIds.map(
+          (userId) => users.find((user) => user.id === userId) ?? undefined
+        )
+      }
+      resolveMentionSuggestions={({ text }) => {
+        let filteredUsers = users;
+
+        if (text) {
+          filteredUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase())
+          );
+        }
+
+        return filteredUsers.map((user) => user.id);
+      }}
+      resolveRoomsInfo={() => []}
+    >
       <RoomProvider id={documentId}>
         <ClientSideSuspense
           fallback={<FullscreenLoader label="Room loading..." />}
